ec5d1a969647ecd9270dbd2d4441f919951a10b4
samratneutroline <samrat.neutroline@gmail.com>
2025-04-24 19:14:44 +0545

    Individual Commented

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,45 +1,47 @@
-import { NextRequest, NextResponse } from "next/server"
-import { getAnnouncementOrOfferById } from "@/db/announcement-offer"
-import { getAppointmentById } from "@/db/appointment"
-import { getUserById } from "@/db/user"
-import { ZodError } from "zod"
-import { prisma } from "@/lib/prisma"
-import { userSchema } from "@/features/user/schemas/schema"
+import { NextRequest, NextResponse } from "next/server";
+
+import { getUserById } from "@/db/user";
+import { ZodError } from "zod";
+import { prisma } from "@/lib/prisma";
+import { userSchema } from "@/features/customer/schemas/schema";
 
 interface ParamsProps {
-  params: Promise<{ id: string }>
+  params: Promise<{ id: string }>;
 }
 
 export async function GET(req: NextRequest, { params }: ParamsProps) {
   try {
-    const { id } = await params
-    const announcement = await getUserById(id)
+    const { id } = await params;
+    const announcement = await getUserById(id);
 
     if (!announcement) {
       return NextResponse.json(
         { error: "User with id not found" },
         { status: 404 }
-      )
+      );
     }
-    return NextResponse.json(announcement, { status: 200 })
+    return NextResponse.json(announcement, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ error: "Failed to fetch user" }, { status: 500 })
+    return NextResponse.json(
+      { error: "Failed to fetch user" },
+      { status: 500 }
+    );
   }
 }
 
 // PUT: Update an existing User
 export async function PUT(req: NextRequest, { params }: ParamsProps) {
   try {
-    const { id } = await params
-    const body = await req.json()
+    const { id } = await params;
+    const body = await req.json();
 
-    const parsedData = userSchema.parse(body)
+    const parsedData = userSchema.parse(body);
 
     // Find the user by email (in a real scenario, use a unique identifier like userId)
-    const existingUser = await getUserById(id)
+    const existingUser = await getUserById(id);
 
     if (!existingUser) {
-      return NextResponse.json({ error: "User not found!" }, { status: 404 })
+      return NextResponse.json({ error: "User not found!" }, { status: 404 });
     }
 
     // Update the user in primsa
@@ -60,51 +62,51 @@ export async function PUT(req: NextRequest, { params }: ParamsProps) {
         },
         role: parsedData.role,
       },
-    })
+    });
 
     return NextResponse.json(
       { message: "User updated successfully!", user: updatedUser },
       { status: 200 }
-    )
+    );
   } catch (error) {
     if (error instanceof ZodError) {
       return NextResponse.json(
         { error: "Validation failed", message: error },
         { status: 400 }
-      )
+      );
     }
     return NextResponse.json(
       { error: "Internal server error", message: error },
       { status: 500 }
-    )
+    );
   }
 }
 
 // DELETE: Delete a User
 export async function DELETE(req: NextRequest, { params }: ParamsProps) {
   try {
-    const { id } = await params
+    const { id } = await params;
 
-    const existingUser = await getUserById(id)
+    const existingUser = await getUserById(id);
 
     if (!existingUser) {
-      return NextResponse.json({ error: "User not found!" }, { status: 404 })
+      return NextResponse.json({ error: "User not found!" }, { status: 404 });
     }
 
     await prisma.user.delete({
       where: { id },
-    })
+    });
 
     return NextResponse.json(
       { message: "User deleted successfully" },
       { status: 200 }
-    )
+    );
   } catch (error) {
     if (error instanceof ZodError) {
       return NextResponse.json(
         { error: "Validation failed", message: error },
         { status: 400 }
-      )
+      );
     }
     return NextResponse.json(
       {
@@ -112,6 +114,6 @@ export async function DELETE(req: NextRequest, { params }: ParamsProps) {
         details: (error as Error).message,
       },
       { status: 500 }
-    )
+    );
   }
 }
